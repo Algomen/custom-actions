3a74330158553e83b0ec560f9ed83febbb8bd882
alvaro <alvarogomen93@gmail.com>
2025-09-14 19:46:30 +0200

    Remove unused comments and prepare for S3 deployment implementation

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -2,13 +2,6 @@ import core from '@actions/core';
 import exec from '@actions/exec';
 import github from '@actions/github';
 
-// 1. Add inputs for bucket name and folder
-// 2. Add output to indicate if cache was used
-// 3. Use the inputs to deploy to the specified S3 bucket and folder
-// 4. Use the output to indicate if cache was used in the cached-deps action
-
-async
-
 function run() {
     core.notice('Deploying to S3 using JavaScript action');
 
