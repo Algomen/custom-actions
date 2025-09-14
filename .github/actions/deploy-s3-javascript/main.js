6b897bf221dab610f32f594a24363aed28fd891f
alvaro <alvarogomen93@gmail.com>
2025-09-14 20:11:26 +0200

    Refactor S3 deployment action to enable input retrieval and add environment variables for AWS credentials

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -5,16 +5,19 @@ import github from '@actions/github';
 function run() {
     core.notice('Deploying to S3 using JavaScript action');
 
-    // try {
-    //     const bucket = core.getInput('bucket-name', { required: true });
-    //     const distFolder = core.getInput('source-dir', { required: true });
-    //     const bucketRegion = core.getInput('bucket-region', { required: true });
+    try {
+        // 1. Get inputs
+        const bucket = core.getInput('bucket-name', { required: true });
+        const distFolder = core.getInput('source-dir', { required: true });
+        const bucketRegion = core.getInput('bucket-region', { required: true });
 
+        // 2. Upload files
+        exec.exec('aws s3 sync ' + distFolder + ' s3://' + bucket + ' --region ' + bucketRegion);
        
 
-    // } catch (error) {
-    //     core.setFailed(`Deployment failed: ${error.message}`);
-    // } 
+    } catch (error) {
+        core.setFailed(`Deployment failed: ${error.message}`);
+    } 
 
 }
 
